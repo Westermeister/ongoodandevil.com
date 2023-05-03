dc2b4e4621624d68f631555d48df80386a9356ad
Westermeister <westermeister12@gmail.com>
2023-05-03 15:00:46 -0700

    Restyle menu icon and add closing button

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -3,12 +3,17 @@
 /** Toggle whether the navbar menu links are shown or not. */
 function toggleMenuLinks() {
 	const links = document.querySelector("#navbar-menu-links");
+	const icon = document.querySelector("#navbar-menu-icon");
 
 	// Initial "style" is empty string because we set "display: none" in stylesheet, NOT inline CSS.
 	// Therefore we only check if display is "block" (i.e. visible) and default to always setting it to "block" if it isn't already.
 	if (links.style.display === "block") {
 		links.style.display = "none";
+		icon.classList.remove("bi-x-lg");
+		icon.classList.add("bi-list");
 	} else {
 		links.style.display = "block";
+		icon.classList.remove("bi-list");
+		icon.classList.add("bi-x-lg");
 	}
 };
